f94790e70ee27e5ba612215a9b25b2b3a30a4c51
Marc Teyssier <marc.teys@gmail.com>
2017-03-27 23:14:46 +0200

    fix(Uduino): Removing Analog reference in hardware code

--- Arduino/libraries/Uduino/examples/Uduino/Uduino.ino
+++ Arduino/libraries/Uduino/examples/Uduino/Uduino.ino
@@ -1,7 +1,4 @@
 // Uduino Default Board
-
-int analogPins[] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A8,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19};
-
 #include<Uduino.h>
 Uduino uduino("uduinoBoard"); // Declare and name your object
 
